bffcf3789b615c876d684a137b2c8df2144706cc
gluha228 <daydreamgluha@gmail.com>
2022-04-17 03:57:29 +0300

    убрал лишнее

--- src/main/java/com/ServingWebContentApplication.java
+++ src/main/java/com/ServingWebContentApplication.java
@@ -1,19 +1,12 @@
 package com;
 
-import com.db.DBRequester;
-import com.parser.AllItemsParser;
-import com.parser.HtmlBodyRequester;
-import com.parser.ItemParser;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
 
 @SpringBootApplication
 public class ServingWebContentApplication {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         SpringApplication.run(ServingWebContentApplication.class, args);
         }
 
