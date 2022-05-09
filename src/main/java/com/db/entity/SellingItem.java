eee4cc2d29102014e4305f8335c4f547ba4efe1a
gluha228 <daydreamgluha@gmail.com>
2022-05-10 01:54:55 +0300

    some fixes

--- src/main/java/com/db/entity/SellingItem.java
+++ src/main/java/com/db/entity/SellingItem.java
@@ -13,7 +13,6 @@ import javax.persistence.*;
 public class SellingItem {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", nullable = false)
     private Long id;
     private String price;
     private String title;
